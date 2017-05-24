f66da8c9a4e1e6135ded39b03480ab34a1d45768
Juan Jose Olivera Rodriguez <juan@genso.com.bo>
2017-05-23 21:26:25 -0400

    removed deprecated BeanInfo annotation

--- src/main/scala/code/model/Model.scala
+++ src/main/scala/code/model/Model.scala
@@ -1,10 +1,6 @@
 package code.model
 
-import scala.beans.BeanInfo
-
-@BeanInfo
 case class Application( customerName : String, creditScore : Int )
 
-@BeanInfo
 case class Approval( message : String, approved : Boolean )
 
